739d65da0b62db6b5c930a1c54b4a44e72517973
andlupa <andlupa@gmail.com>
2026-07-31 21:25:46 +0200

    Primo commit: API CongMan

---
+++ ApiKeyMiddleware.cs
@@ -0,0 +1,32 @@
+﻿public class ApiKeyMiddleware
+{
+    private const string API_KEY_HEADER = "X-Api-Key";
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _config;
+
+    public ApiKeyMiddleware(RequestDelegate next, IConfiguration config)
+    {
+        _next = next;
+        _config = config;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(API_KEY_HEADER, out var extractedKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("API Key mancante.");
+            return;
+        }
+
+        var apiKey = _config["ApiKey"];
+        if (!apiKey.Equals(extractedKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("API Key non valida.");
+            return;
+        }
+
+        await _next(context);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowClient", policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 var app = builder.Build();
+
+app.UseStaticFiles();
+
+// dopo aver costruito l'app (var app = builder.Build();):
+app.UseCors("AllowClient");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ApiKeyMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
